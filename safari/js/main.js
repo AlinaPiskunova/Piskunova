9986b73bf6839a09adb173f28ea41c690db22d43
AlinaPiskunova <piskunovaay@gmail.com>
2019-02-13 17:52:54 +0200

    update

--- safari/js/main.js
+++ safari/js/main.js
@@ -2,10 +2,10 @@
     $(function () {
 
         var w = $(window),
-            b1 = $('.relative>.decoration:nth-child(1)'),
-            b2 = $('.relative>.decoration:nth-child(2)'),
+            b1 = $('.decoration:nth-child(1)'),
+            b2 = $('.decoration:nth-child(2)'),
             img = $('.plane:first-of-type'),
-        b1b = b1.position().top,
+            b1b = b1.position().top,
             b2b = b2.position().top;
         // console.log(img);
         // imgPos = img.position.right;
@@ -23,3 +23,56 @@
         });
     });
 })(jQuery);
+var map,
+    infoWindowContent = document.getElementsByClassName('europe')[0].outerHTML,
+    infoWindowContentAfrica = document.getElementsByClassName('africa')[0].outerHTML,
+    infoWindowContentUSA = document.getElementsByClassName('usa')[0].outerHTML;
+
+function initMap() {
+    var Europe = {lat: 48.989800, lng: 22.989173},
+        Africa = {lat: 5.295441, lng: 22.353199},
+        usa = {lat: 37.597996, lng: -110.762886},
+        atlantica = {lat: 23.665940, lng: -39.290077},
+        image = 'img/plane111.png',
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: atlantica,
+            zoom: 3
+        }),
+        marker = new google.maps.Marker({
+            position: Europe,
+            map: map,
+            icon: image,
+            title: 'Europe'
+        }),
+        markerAfrica = new google.maps.Marker({
+            position: Africa,
+            map: map,
+            icon: image,
+            title: ' Africa'
+        }),
+        markerUSA = new google.maps.Marker({
+            position: usa,
+            map: map,
+            icon: image,
+            title: 'USA'
+        }),
+        infowindow = new google.maps.InfoWindow({
+            content: infoWindowContent
+        }),
+        infowindowAfrica = new google.maps.InfoWindow({
+            content: infoWindowContentAfrica
+        }),
+        infowindowUSA = new google.maps.InfoWindow({
+            content: infoWindowContentUSA
+        });
+    marker.setMap(map);
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+    markerAfrica.addListener('click', function () {
+        infowindowAfrica.open(map, markerAfrica);
+    });
+    markerUSA.addListener('click', function () {
+        infowindowUSA.open(map, markerUSA);
+    });
+}
